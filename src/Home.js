10c9dc58ea99d950efb664e80cfbdb81bc432ae2
Avinashhola <avinash.hola9@outlook.com>
2023-11-05 23:08:51 +0530

    some changes

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = ()=>{
+    return (
+<div>
+    <h1>Home for all</h1>
+    <p>am here to rule!<hr/>
+    am here to rule!       <hr/>
+    am here to rule!           <hr/>
+    am here to rule!               <hr/>
+    </p>
+
+</div>
+    )
+}
+export default Home
