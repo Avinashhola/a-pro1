08a87b5038f41bcd29cd07b9b7844e2709934041
Avinashhola <avinash.hola9@outlook.com>
2023-11-05 23:27:59 +0530

    styling 3

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import './Home.css'
 const Home = ()=>{
     return (
-<div>
+<div className="newstyle">
     <h1>Home for all</h1>
     <p>am here to rule!<hr/>
     am here to rule!       <hr/>
